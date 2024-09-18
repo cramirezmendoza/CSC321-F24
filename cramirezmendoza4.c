202278ed43912500ca9621de2b4144df45d16034
Camila Ramirez Mendoza <cramirezmendoza@gmail.com>
2024-09-18 01:29:33 +0000

    finished lab

--- cramirezmendoza4.c
+++ cramirezmendoza4.c
@@ -14,16 +14,54 @@ int main(void)
 	printf("enter an integar: ");
 	scanf("%d",&x);
 
+	printf("enter a double: ");
+        scanf("%lf", &y);
+
+	printf("Enter a character: ");
+        scanf(" %c", &a);
+
+	printf("Enter a float: ");
+        scanf("%f", &z);
+
+
 	if(x > 10)
 	{
-		printf("You wrote an number larger than 5 \n");
+		printf("You wrote an number larger than 10 \n");
 	}
-	else if(x == 5)
+	else if(x == 10)
 	{
-		printf("You input 5 \n");
+		printf("You input is 10 \n");
 	}
 	else
 	{
-		printf("You entered a number less than 5 \n");
+		printf("You entered a number less than 10 \n");
+	}
+
+	if(y > 43.2)
+	{
+		printf("You wrote a number bigger than 43.2 \n");
+	}
+	else
+	{
+		printf("You entered a number less than 43.2");
 	}
 	
+	if(z > 0) {
+        printf("You entered a positive float\n");
+   	 }
+   	 else if(z < 0) {
+        printf("You entered a negative float\n");
+   	 }
+   	 else {
+        printf("You entered zero\n");
+   	 }
+	
+	 if(a == 'a') {
+        printf("You entered 'a'\n");
+   	 }
+   	 else {
+       	 	printf("You entered a different character\n");
+   	 }
+	 
+	return EXIT_SUCCESS;
+}	
