de2a9eb3e722dd5a64f6fccf434053b39f7e1d4f
Camila Ramirez Mendoza <cramirezmendoza@gmail.com>
2024-10-23 01:02:31 +0000

    finished lab 7

---
+++ cramirezmendoza7.py
@@ -0,0 +1,16 @@
+x = 6
+y = 5.5
+z = "California"
+
+if z == "California":
+    print("Variable z is California")
+else:
+    print("variable z is not California")
+if x > 5:
+    print("x is greater than 5")
+else:
+    print("x is less than 5")
+if y == 5.5:
+    print("y is equal to 5.5")
+else:
+    print("y is not equal to 5.5")
