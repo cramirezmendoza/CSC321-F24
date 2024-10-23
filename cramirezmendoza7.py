d98be442e905dc3668f8c1d834e521d3eba3510e
Camila Ramirez Mendoza <cramirezmendoza@gmail.com>
2024-10-23 01:06:48 +0000

    new variables

--- cramirezmendoza7.py
+++ cramirezmendoza7.py
@@ -1,8 +1,8 @@
 # Camila Ramirez
 # CSC 321 - lab 7
 
-x = 6
-y = 5.5
+x = 9
+y = 7.3
 z = "California"
 
 if z == "California":
@@ -13,7 +13,7 @@ if x > 5:
     print("x is greater than 5")
 else:
     print("x is less than 5")
-if y == 5.5:
-    print("y is equal to 5.5")
+if y == 7.3:
+    print("y is equal to 7.3")
 else:
-    print("y is not equal to 5.5")
+    print("y is not equal to 7.3")
